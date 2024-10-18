2a2bba763e101b6901356af56b636bbf934e2bd1
ramon.duarte <ramon.duarte@PAT-000112>
2024-10-18 09:41:31 -0300

    minichat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MiniChatComponent } from "./components/mini-chat/mini-chat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MiniChatComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'vvs-disk';
---
+++ src/app/utils/enumUtils.ts
@@ -0,0 +1,14 @@
+export const EstadoAtendimento = {
+    Inicio: 'Inicio',
+    FluxoIniciado: 'FluxoIniciado',
+    SolicitandoDados: 'SolicitandoDados',
+    ConfirmandoDados: 'ConfirmandoDados',
+    DadosConfirmados: 'DadosConfirmados',
+    DadosCorretos: 'DadosCorretos',
+    SolicitandoTicket: 'SolicitandoTicket',
+    JaAbriuTicket: 'JaAbriuTicket',
+    TicketNaoAberto: 'TicketNaoAberto',
+    Desistir: 'Desistir',
+    ChatSuporte: 'ChatSuporte'
+  };
+  
